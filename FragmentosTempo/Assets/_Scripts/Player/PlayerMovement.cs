cfce449582692430dae2fddb1a3a482902c56344
Leoorfb <leorfb3@gmail.com>
2025-04-25 16:09:23 -0300

    Player Dash

--- FragmentosTempo/Assets/_Scripts/Player/PlayerMovement.cs
+++ FragmentosTempo/Assets/_Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float walkSpeed = 5f;
+
+    [SerializeField] private float dashSpeed = 12f;
+    [SerializeField] private float dashCooldownTime = 1.5f;
+    [SerializeField] private float dashDuration = .2f;
+
+    private bool isDashing = false;
+    private float dashTimer = 0;
+
+    private float moveSpeed;
 
     private InputAction moveInput;
     private InputAction dashInput;
@@ -15,11 +24,13 @@ public class PlayerMovement : MonoBehaviour
 
 
     private Vector3 moveDirection = Vector3.zero;
+    private Vector3 moveDirectionMag = Vector3.zero;
 
     private void Awake()
     {
         playerInputAction = new PlayerInputAction();
         rb = GetComponent<Rigidbody>();
+        moveSpeed = walkSpeed;
     }
 
     private void OnEnable()
@@ -29,24 +40,54 @@ public class PlayerMovement : MonoBehaviour
 
         dashInput = playerInputAction.Player.Dash;
         dashInput.Enable();
+        dashInput.performed += Dash;
     }
 
     private void OnDisable()
     {
         moveInput.Disable();
+        dashInput.Disable();
     }
 
 
     private void Update()
     {
-        moveDirection.x = moveInput.ReadValue<Vector2>().x * speed;
-        moveDirection.z = moveInput.ReadValue<Vector2>().y * speed;
+        if (!isDashing)
+        {
+            moveDirection.x = moveInput.ReadValue<Vector2>().x;
+            moveDirection.z = moveInput.ReadValue<Vector2>().y;
+        }
+
+        moveDirectionMag.x = moveDirection.x * moveSpeed;
+        moveDirectionMag.z = moveDirection.z * moveSpeed;
 
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = moveDirection;
+        rb.velocity = moveDirectionMag;
     }
 
+    private void Dash(InputAction.CallbackContext obj)
+    {
+        Dash();
+    }
+
+    private void Dash()
+    {
+        if (isDashing || dashTimer - Time.time > dashCooldownTime) return;
+
+        isDashing = true;
+        dashTimer = Time.time;
+        moveSpeed = dashSpeed;
+
+        StartCoroutine(DashCooldown());
+    }
+
+    private IEnumerator DashCooldown()
+    {
+        yield return new WaitForSeconds(dashDuration);
+        isDashing = false;
+        moveSpeed = walkSpeed;
+    }
 }
