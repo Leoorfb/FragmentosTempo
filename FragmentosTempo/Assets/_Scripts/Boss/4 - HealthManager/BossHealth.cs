36a095e63a5b1d7b89d7b3fef02dab728510d7c5
MatheusFF96 <matheusfreitasfernandes18@gmail.com>
2025-05-11 18:14:46 -0300

    HUD Skill's e Health do Player
    Ideia base dos HUD's do player, e atualização da Health Bar do Boss

--- FragmentosTempo/Assets/_Scripts/Boss/4 - HealthManager/BossHealth.cs
+++ FragmentosTempo/Assets/_Scripts/Boss/4 - HealthManager/BossHealth.cs
@@ -2,17 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossHealth : MonoBehaviour
 {
     public int maxHealth = 100;                                         // Quantidade máxima de vida.
     private int currentHealth;                                          // Vida atual do Boss.
     private BossHealthBarUI healthBarUI;                                // Referência para o script que controla a UI da barra de vida.
+    private string bossName;                                            // Nome do Boss atual.
 
     void Start()
     {
         currentHealth = maxHealth;                                      // Define a vida atual como a vida máxima.
         healthBarUI = BossHealthManager.Instance.SpawnBar();            // Instancia a barra de vida e retorna a referência dela.
+
+        string sceneName = SceneManager.GetActiveScene().name;          // Define automaticamente o nome do Boss com base na cena atual.
+        switch (sceneName)
+        {
+            case "BossTrice":
+                bossName = "Triceratops";
+                break;
+            case "BossFornalha":
+                bossName = "Fornalha";
+                break;
+            case "BossFinal":
+                bossName = "Final Boss";
+                break;
+        }
+
+        healthBarUI.DefNameBoss(bossName);                              // Atualiza o nome no UI.
         UpdateLifeBar();                                                // Chama o método para atualizar a barra de vida na tela com os valores atuais.
     }
 
--- FragmentosTempo/Assets/_Scripts/Player/PlayerMovement.cs
+++ FragmentosTempo/Assets/_Scripts/Player/PlayerMovement.cs
@@ -2,19 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float walkSpeed = 5f;
 
+    [Header("Dash Settings")]
     [SerializeField] private float dashSpeed = 12f;
     [SerializeField] private float dashCooldownTime = 1.5f;
     [SerializeField] private float dashDuration = .2f;
+    [SerializeField] private Image dashCDOverlay;
+
+    [Header("Attack Settings")]
+    [SerializeField] private float attackCooldownTime = 1.0f;
+    [SerializeField] private Image attackCDOverlay;
+
+    [Header("Life Potion Settings")]
+    [SerializeField] private float lifePotionCooldownTime = 5.0f;
+    [SerializeField] private Image lifePotionCDOverlay;
 
     private bool isDashing = false;
-    private float dashTimer = 0;
-
     private float moveSpeed;
+
+    private float dashTimer = 0;
+    private float attackTimer = 0;
+    private float lifePotionTimer = 0;
 
     private InputAction moveInput;
     private InputAction dashInput;
@@ -31,6 +44,10 @@ public class PlayerMovement : MonoBehaviour
         playerInputAction = new PlayerInputAction();
         rb = GetComponent<Rigidbody>();
         moveSpeed = walkSpeed;
+
+        dashTimer = Time.time - dashCooldownTime;
+        attackTimer = Time.time - attackCooldownTime;
+        lifePotionTimer = Time.time - lifePotionCooldownTime;
     }
 
     private void OnEnable()
@@ -61,6 +78,9 @@ public class PlayerMovement : MonoBehaviour
         moveDirectionMag.x = moveDirection.x * moveSpeed;
         moveDirectionMag.z = moveDirection.z * moveSpeed;
 
+        UpdateCooldownUI(dashCDOverlay, dashTimer, dashCooldownTime);
+        UpdateCooldownUI(attackCDOverlay, attackTimer, attackCooldownTime);
+        UpdateCooldownUI(lifePotionCDOverlay, lifePotionTimer, lifePotionCooldownTime);
     }
 
     private void FixedUpdate()
@@ -75,10 +95,9 @@ public class PlayerMovement : MonoBehaviour
 
     private void Dash()
     {
-        if (isDashing || dashTimer - Time.time > dashCooldownTime) return;
+        if (isDashing || Time.time - dashTimer < dashCooldownTime) return;
 
         isDashing = true;
-        dashTimer = Time.time;
         moveSpeed = dashSpeed;
 
         StartCoroutine(DashCooldown());
@@ -89,5 +108,33 @@ public class PlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(dashDuration);
         isDashing = false;
         moveSpeed = walkSpeed;
+        dashTimer = Time.time;
+    }
+
+    private void UpdateCooldownUI(Image overlay, float timer, float cooldown)
+    {
+        if (overlay == null) return;
+
+        float timeElapsed = Time.time - timer;
+        float fill = Mathf.Clamp01(timeElapsed / cooldown);
+        overlay.fillAmount = fill;
+    }
+
+    public void UseAttack()
+    {
+        if (Time.time - attackTimer >= attackCooldownTime)
+        {
+            attackTimer = Time.time;
+            // Lógica de Ataque.
+        }
+    }
+
+    public void UseLifePotion()
+    {
+        if (Time.time - lifePotionTimer >= lifePotionCooldownTime)
+        {
+            lifePotionTimer = Time.time;
+            // Lógica de cura.
+        }
     }
 }
